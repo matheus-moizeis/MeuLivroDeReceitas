471dcbd3e9439a4498200459536297c32b7dc5c2
Matheus Moizeis <matheusmoizeis@outlook.com>
2023-10-15 02:03:59 -0300

    adicionando as classes de migration da tabela usuario do banco de dados

---
+++ src/Backend/MeuLivroDeReceitas.Infrastructure/Migrations/VersaoBase.cs
@@ -0,0 +1,15 @@
+﻿using FluentMigrator;
+using FluentMigrator.Builders.Create.Table;
+
+namespace MeuLivroDeReceitas.Infrastructure.Migrations;
+
+
+public static class VersaoBase 
+{
+    public static ICreateTableColumnOptionOrWithColumnSyntax InserirColunasPadrao(ICreateTableWithColumnOrSchemaOrDescriptionSyntax tabela)
+    {
+        return tabela
+            .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+            .WithColumn("DataCriacao").AsDateTime().NotNullable();
+    }
+}
