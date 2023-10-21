4b29f3af75366c70e70374a7df5932e27073c709
Matheus Moizeis <matheusmoizeis@outlook.com>
2023-10-21 16:17:20 -0300

    Adicionado regra para não cadastrar mais de um usuario por email.

--- src/Backend/MeuLivroDeReceitas.Application/UseCases/Usuario/Registrar/RegistrarUsuarioUseCase .cs
+++ src/Backend/MeuLivroDeReceitas.Application/UseCases/Usuario/Registrar/RegistrarUsuarioUseCase .cs
@@ -4,30 +4,33 @@ using MeuLivroDeReceitas.Application.Servicos.Token;
 using MeuLivroDeReceitas.Comunicacao.Requisicoes;
 using MeuLivroDeReceitas.Comunicacao.Respostas;
 using MeuLivroDeReceitas.Domain.Repositorios;
+using MeuLivroDeReceitas.Exceptions;
 using MeuLivroDeReceitas.Exceptions.ExceptionsBase;
 
 namespace MeuLivroDeReceitas.Application.UseCases.Usuario.Registrar;
 
 public class RegistrarUsuarioUseCase : IRegistrarUsuarioUseCase
 {
+    private readonly IUsuarioReadOnlyRepositorio _usuarioReadOnlyRepositorio;
     private readonly IUsuarioWriteOnlyRepositorio _repositorio;
     private readonly IMapper _mapper;
     private readonly IUnidadeDeTrabalho _unidadeDeTrabalho;
     private readonly EncriptadorDeSenha _encriptadorDeSenha;
     private readonly TokenController _tokenController;
 
-    public RegistrarUsuarioUseCase(IUsuarioWriteOnlyRepositorio repositorio, IMapper mapper, IUnidadeDeTrabalho unidadeDeTrabalho, EncriptadorDeSenha encriptadorDeSenha, TokenController tokenController)
+    public RegistrarUsuarioUseCase(IUsuarioWriteOnlyRepositorio repositorio, IMapper mapper, IUnidadeDeTrabalho unidadeDeTrabalho, EncriptadorDeSenha encriptadorDeSenha, TokenController tokenController, IUsuarioReadOnlyRepositorio usuarioReadOnlyRepositorio)
     {
         _repositorio = repositorio;
         _mapper = mapper;
         _unidadeDeTrabalho = unidadeDeTrabalho;
         _encriptadorDeSenha = encriptadorDeSenha;
         _tokenController = tokenController;
+        _usuarioReadOnlyRepositorio = usuarioReadOnlyRepositorio;
     }
 
     public async Task<RespostaUsuarioRegistradoJson> Executar(RequisicaoRegistrarUsuarioJson requisicao)
     {
-        Validar(requisicao);
+        await Validar(requisicao);
 
         var entidade = _mapper.Map<Domain.Entidades.Usuario>(requisicao);
 
@@ -45,10 +48,17 @@ public class RegistrarUsuarioUseCase : IRegistrarUsuarioUseCase
         };
     }
 
-    private void Validar(RequisicaoRegistrarUsuarioJson requisicao)
+    private async Task Validar(RequisicaoRegistrarUsuarioJson requisicao)
     {
         var validator = new RegistrarUsuarioValidator();
         var resultado = validator.Validate(requisicao);
+
+        var existeusuarioComEmail = await _usuarioReadOnlyRepositorio.ExisteUsuarioComEmail(requisicao.Email);
+
+        if (existeusuarioComEmail)
+        {
+            resultado.Errors.Add(new FluentValidation.Results.ValidationFailure("email", ResourceMensagensDeErro.EMAIL_JA_REGISTRADO));
+        }
 
         if (!resultado.IsValid)
         {
